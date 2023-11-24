97fd7070df59d13dd5cea43a5e7a529c987730c0
Alperen Türksoy <alperen.turksoy@aysanraf.com.tr>
2023-11-24 17:16:05 +0300

    plannedList_completed

--- AysanRaf.NakliyeMontaj.ng/src/app/shared/planned/components/list/planned.list.component.ts
+++ AysanRaf.NakliyeMontaj.ng/src/app/shared/planned/components/list/planned.list.component.ts
@@ -1,5 +1,31 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from "@angular/core";
 import { DatatableComponent } from "@swimlane/ngx-datatable";
+import { MatTableModule } from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  hmm: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H',hmm:10 },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', hmm: 10 },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', hmm: 10 },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', hmm: 10 },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B', hmm: 10 },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', hmm: 10 },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', hmm: 10 },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', hmm: 10 },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', hmm: 10 },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', hmm: 10 },
+];
+
+/**
+ * @title Basic use of `<table mat-table>`
+ */
 
 @Component({
 
@@ -11,7 +37,20 @@ import { DatatableComponent } from "@swimlane/ngx-datatable";
 
 export class PlannedListComponent implements OnInit {
   @ViewChild(DatatableComponent)
-    table!: DatatableComponent;
+  table!: DatatableComponent;
+
+
+  editRow(row: any) {
+    // Burada seçilen satırı düzenlemek için gerekli işlemleri yapabilirsiniz.
+    console.log('uu', row.name);
+  }
+
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','hmm'];
+  dataSource = ELEMENT_DATA;
+  clickedRows = new Set<PeriodicElement>();
+
+
 
   @Input() showFilter: boolean = true;
 
